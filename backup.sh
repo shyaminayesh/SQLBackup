6245dff3c880c03163f90f362873a0f5d7ec7d02
shyaminayesh <admin@shyamin.com>
2013-12-20 09:27:08 +0530

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# DATABASE CONNECTION DETAILS
+DBHOST=localhost
+DBNAME=database
+DBUSER=root
+DBPASS=pass
+
+# FTP SERVER DETAILS
+FTPHOST=123.123.123.123
+FTPUSER=username@domain.tld
+FTPPASS=password
+FTPFOLD=/home/
+
+# MAKE FILE NAME
+FileName=$(date +"%Y-%b-%d-%H:%M")_$DBNAME;
+
+# GET BACKUP USING MYSQLDUMP COMMAND
+mysqldump --user=$DBUSER --password=$DBPASS --host=$DBHOST $DBNAME > $FileName.sql
+
+# TRANSFER BACKUP FILE TP FTP SERVER
+ftp -n -v $FTPHOST << EOT
+ascii
+prompt
+user $FTPUSER $FTPPASS
+mput *_$DBNAME.sql $FTPFOLD
+bye
+EOT
+
+# DELETE THE BACKUP FILE FROM LOCAL HOST
+rm -rf *_$DBNAME.sql
